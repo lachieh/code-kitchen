8938fd5f0f15a49f97cc987f4c9c8e5317379093
Lachlan Heywood <lachlan@realtheory.io>
2020-06-23 14:17:33 -0400

    add recipeform component and route

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Recipes from './components/Recipes';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import RecipeDetails from './components/RecipeDetails';
+import RecipeForm from './components/RecipeForm';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className="App">
         <Switch>
           <Route path="/" exact component={Recipes} />
+          <Route path="/recipes/new" exact component={RecipeForm} />
           <Route path="/recipes/:id" component={RecipeDetails} />
         </Switch>
       </div>
